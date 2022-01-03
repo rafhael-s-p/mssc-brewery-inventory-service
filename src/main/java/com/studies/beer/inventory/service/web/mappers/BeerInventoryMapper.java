9e34e4cdb0aba3e16fb35817734c83e05d3b04f4
Rafhael <rafhael.dev@gmail.com>
2022-01-03 09:03:41 -0300

    first commit

---
+++ src/main/java/com/studies/beer/inventory/service/web/mappers/BeerInventoryMapper.java
@@ -0,0 +1,16 @@
+package com.studies.beer.inventory.service.web.mappers;
+
+import com.studies.beer.inventory.service.domain.BeerInventory;
+import com.studies.beer.inventory.service.web.model.BeerInventoryDto;
+import org.mapstruct.Mapper;
+
+/**
+ * Created by jt on 2019-05-31.
+ */
+@Mapper(uses = {DateMapper.class})
+public interface BeerInventoryMapper {
+
+    BeerInventory beerInventoryDtoToBeerInventory(BeerInventoryDto beerInventoryDTO);
+
+    BeerInventoryDto beerInventoryToBeerInventoryDto(BeerInventory beerInventory);
+}
