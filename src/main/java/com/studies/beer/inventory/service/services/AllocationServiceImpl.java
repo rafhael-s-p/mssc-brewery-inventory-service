6530aded3d7c20c04f6ad63851e7763f42f849d3
Rafhael <rafhael.dev@gmail.com>
2022-02-24 11:07:53 -0300

    fixing zero onhand

--- src/main/java/com/studies/beer/inventory/service/services/AllocationServiceImpl.java
+++ src/main/java/com/studies/beer/inventory/service/services/AllocationServiceImpl.java
@@ -57,9 +57,12 @@ public class AllocationServiceImpl implements AllocationService {
             } else if (inventory > 0) { //partial allocation
                 beerOrderLine.setQuantityAllocated(allocatedQty + inventory);
                 beerInventory.setQuantityOnHand(0);
+            }
 
+            if (beerInventory.getQuantityOnHand() == 0) {
                 beerInventoryRepository.delete(beerInventory);
             }
+
         });
 
     }
